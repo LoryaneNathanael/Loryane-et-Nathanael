4d4dbc44b70da4a450582da96f961097148f5135
LoryaneNathanaël <loryane.aknine@gmail.com>
2024-10-27 18:00:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,15 +7,20 @@ document.addEventListener('DOMContentLoaded', function() {
     const backgroundMusic = document.getElementById('background-music');
     let step = 1;
 
+    // Lancer la musique au clic sur le bouton
     playMusicButton.addEventListener('click', function() {
         backgroundMusic.play();
     });
 
+    // Animation d'ouverture de la carte
     cardContainer.addEventListener('click', function() {
         if (step === 1) {
+            // Cache l'image de la couverture et affiche les pages gauche et droite
             card.style.display = 'none';
             centreGauche.style.display = 'block';
             centreDroite.style.display = 'block';
+
+            // Animation pour ouvrir la carte
             centreGauche.style.transform = 'rotateY(0deg)'; // Ouvrir la page gauche
             centreDroite.style.transform = 'rotateY(0deg)'; // Ouvrir la page droite
             step++;
