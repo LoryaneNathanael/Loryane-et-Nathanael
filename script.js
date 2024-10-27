c3969532f4e0edde519f8ebb576511c5a1022552
LoryaneNathanaël <loryane.aknine@gmail.com>
2024-10-27 20:17:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const backgroundMusic = document.getElementById('background-music');
-    const playMusicButton = document.getElementById('play-music');
     const cover = document.querySelector('.book-cover');
     const centreDroite = document.getElementById('centre_droite');
     let step = 1;
-
-    // Charger les images
-    function preloadImages(callback) {
-        const images = ['exterieur_recto.png', 'centre_droite.png'];
-        let loadedImages = 0;
-
-        images.forEach((src) => {
-            const img = new Image();
-            img.src = src;
-            img.onload = () => {
-                loadedImages++;
-                if (loadedImages === images.length) callback();
-            };
-        });
-    }
-
-    preloadImages(() => {
-        document.getElementById('loader').style.display = 'none';
-        document.getElementById('background-video').style.display = 'block';
-    });
-
-    // Musique
-    playMusicButton.addEventListener('click', function() {
-        backgroundMusic.play();
-    });
 
     // Animation de glissement pour centre_droite
     cover.addEventListener('click', function() {
