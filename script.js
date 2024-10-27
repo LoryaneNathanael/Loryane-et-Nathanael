41d776a8c4253fe045337dce0f393fa2932243dc
LoryaneNathanaël <loryane.aknine@gmail.com>
2024-10-27 22:27:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const backgroundMusic = document.getElementById('background-music');
-    const playMusicButton = document.getElementById('play-music');
     const exterieurRecto = document.getElementById('exterieur_recto');
     const centreDouble = document.getElementById('centre_double');
-    const halfView = document.querySelector('.half-view');
     let step = 1;
-
-    // Démarrer la musique
-    playMusicButton.addEventListener('click', function() {
-        backgroundMusic.play();
-    });
 
     // Animation au clic pour montrer `centre_double`
     exterieurRecto.addEventListener('click', function() {
@@ -17,9 +9,8 @@ document.addEventListener('DOMContentLoaded', function() {
             exterieurRecto.classList.add('open-book'); // Applique l'effet de livre
             setTimeout(() => {
                 exterieurRecto.style.display = 'none'; // Cache `exterieur_recto`
-                halfView.style.display = 'block'; // Affiche le conteneur `half-view`
                 centreDouble.classList.remove('hidden'); // Affiche `centre_double`
-            }, 500); // Temporisation pour l'effet d'ouverture
+            }, 1000); // Temporisation pour l'effet d'ouverture
             step++;
         }
     });
