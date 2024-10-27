5ec8085f84e9cb6bdb06538e1caa9125f3d07fa0
LoryaneNathanaël <loryane.aknine@gmail.com>
2024-10-27 20:21:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const cover = document.querySelector('.book-cover');
     const centreDroite = document.getElementById('centre_droite');
-    let step = 1;
 
-    // Animation de glissement pour centre_droite
-    cover.addEventListener('click', function() {
-        if (step === 1) {
-            cover.style.transform = 'rotateY(-180deg)'; // Ouvre la couverture
-            centreDroite.style.display = 'block'; // Affiche l'intérieur (centre_droite)
-            step++;
-        } else if (step === 2) {
-            // Fait glisser centre_droite vers la droite
-            centreDroite.classList.add('slide-out-right');
-            step++;
-        }
+    // Glissement vers la droite au clic
+    centreDroite.addEventListener('click', function() {
+        centreDroite.classList.add('slide-out-right');
     });
 });
